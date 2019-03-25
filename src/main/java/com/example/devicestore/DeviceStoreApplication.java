2e4b62ff7f9fba266377f150663526023962a425
yura8822 <yura8822m@gmail.com>
2019-03-25 19:57:54 +0300

    init

---
+++ src/main/java/com/example/devicestore/DeviceStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.devicestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceStoreApplication.class, args);
+	}
+
+}
