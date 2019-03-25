e7467a0618c5caa5cc859eea40d71a951fad6e1c
yura8822 <yura8822m@gmail.com>
2019-03-25 21:27:42 +0300

    task_1

--- src/main/java/com/example/devicestore/DeviceStoreApplication.java
+++ src/main/java/com/example/devicestore/DeviceStoreApplication.java
@@ -1,13 +1,20 @@
 package com.example.devicestore;
 
+import com.example.devicestore.test1.ExampleHelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DeviceStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeviceStoreApplication.class, args);
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("testconfig.xml");
+		ExampleHelloWorld exampleHelloWorld = applicationContext.getBean("exampleHelloWorld", ExampleHelloWorld.class);
+		System.out.println(exampleHelloWorld.getText());
+
 	}
 
 }
---
+++ src/main/java/com/example/devicestore/test1/ExampleHelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.devicestore.test1;
+
+public class ExampleHelloWorld {
+    private String text;
+
+    public ExampleHelloWorld() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
