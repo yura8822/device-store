5b89e2a65a31557f3785a3b743ea0fe24087b8ac
yura8822 <yura8822m@gmail.com>
2019-04-11 22:00:22 +0300

    add @Qery

--- src/main/java/com/example/task_example_DB/entity/Expence.java
+++ src/main/java/com/example/task_example_DB/entity/Expence.java
@@ -6,8 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.LinkedList;
-import java.util.List;
+import java.math.BigDecimal;
 
 
 @Entity
@@ -17,6 +16,9 @@ public class Expence implements Serializable {
     @SequenceGenerator(name = "expence_seq", sequenceName = "expence_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "expence_seq")
     private long id;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
 
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
@@ -31,9 +33,10 @@ public class Expence implements Serializable {
     public Expence() {
     }
 
-    public Expence(Category category, User user) {
+    public Expence(Category category, User user, BigDecimal amount) {
         this.user = user;
         this.category = category;
+        this.amount = amount;
     }
 
     public long getId() {
@@ -54,5 +57,13 @@ public class Expence implements Serializable {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
     }
 }
