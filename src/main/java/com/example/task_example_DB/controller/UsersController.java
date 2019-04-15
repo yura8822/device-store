a284f4719e760cd894012c78f678a663463bb376
yura8822 <yura8822m@gmail.com>
2019-04-15 22:49:09 +0300

    bag_ajax

--- src/main/java/com/example/task_example_DB/controller/UsersController.java
+++ src/main/java/com/example/task_example_DB/controller/UsersController.java
@@ -5,8 +5,10 @@ import com.example.task_example_DB.dao.UserServiceImpl;
 import com.example.task_example_DB.entity.User;
 import com.example.task_example_DB.repo.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -48,6 +50,12 @@ public class UsersController {
     @PostMapping
     public User createUser(@RequestBody User userCreateDTO) {
         return userService.createUser(userCreateDTO);
+    }
+
+    @PostMapping("/users/se")
+    public ResponseEntity<?> createUserAjax(@Valid @RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
+
     }
 
 }
