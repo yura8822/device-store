4cf1b3b96ae6da6a0ce7c6ae19a11b068c9b90f1
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2019-04-16 23:22:59 +0300

    Fixed JSP and thymeleaf

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,15 @@
-(function(){
-    console.log("Hello World!");
-})();
+$(document).ready(function () {
+    getIter();
+});
+
+function getIter() {
+    $.ajax({
+        url: "/iter",
+        type: 'GET',
+        dataType: 'json', // added data type
+        success: function(res) {
+
+            $("#iter").text(res);
+        }
+    });
+}
