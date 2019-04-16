11f23f40a9fc9a7a67ae0335adb4095b5642faa5
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2019-04-16 21:13:47 +0300

    Fixed JSP and thymeleaf

--- src/main/java/com/example/task_example_DB/JPSConfiguration.java
+++ src/main/java/com/example/task_example_DB/JPSConfiguration.java
@@ -11,6 +11,10 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 public class JPSConfiguration implements WebMvcConfigurer {
 
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -23,5 +27,13 @@ public class JPSConfiguration implements WebMvcConfigurer {
     public void configureDefaultServletHandling(
             DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                    CLASSPATH_RESOURCE_LOCATIONS);
+        }
     }
 }
